32eaef7f1022e50ae093c3e3ca901a1bf975a6a4
kromer52 <ilonakromer52@googlemail.com>
2025-06-30 13:06:57 +0200

    added Axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Weather from "./Weather";
 import "./App.css";
+import axios from "axios";
 
 export default function App() {
   const [city, setCity] = useState("");
@@ -11,21 +12,21 @@ export default function App() {
     const apiKey = "303634af30at1e0bobd77c2b1f682f81";
     const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setWeatherData({
-          temperature: Math.round(data.main.temp),
-          description: data.weather[0].description,
-          humidity: data.main.humidity,
-          wind: data.wind.speed,
-          city: data.name,
-        });
-      })
-      .catch((error) => {
-        console.error("Error fetching weather:", error);
-        setWeatherData(null);
-      });
+   axios.get(apiUrl)
+  .then((response) => {
+    const data = response.data;
+    setWeatherData({
+      temperature: Math.round(data.main.temp),
+      description: data.weather[0].description,
+      humidity: data.main.humidity,
+      wind: data.wind.speed,
+      city: data.name,
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching weather:", error);
+    setWeatherData(null);
+  });
   }
 
   return (
